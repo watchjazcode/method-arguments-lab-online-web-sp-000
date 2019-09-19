87cccfb6922656bd441a939e595dedf7eb0225c4
Jazmin M <jazminmerckel@gmail.com>
2019-09-19 23:13:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-def introduction(Josh)
+def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(Dan, Ember.js)
+def introduction_with_language(name, language)
   puts "Hi, my name is Dan and I am learning to program in Ember.js.#{language}"
 end
